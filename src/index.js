70a29e0ade01a336c2e423bb4c26a7993344488c
Koki Tanaka <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-07-10 00:21:20 +0900

    paiinting

---
+++ src/index.js
@@ -0,0 +1,52 @@
+/* define valuable */
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext('2d');
+canvas.setAttribute("width", window.parent.screen.width);
+canvas.setAttribute("height", window.parent.screen.height);
+var drawing = false;
+var oldPos;
+context.strokeStyle = "#000000";
+context.lineWidth = 5;
+context.lineJoin = "round";
+context.lineCap = "round";
+
+/* define function */
+function scrollX() {
+    return document.documentElement.scrollLeft || document.body.scrollLeft;
+}
+
+function scrollY() {
+    return document.documentElement.scrollTop || document.body.scrollTop;
+}
+
+function getPosT(event) {
+    var mouseX = event.clientX ;
+    var mouseY = event.clientY;
+    return { x: mouseX, y: mouseY };
+}
+
+/* event listebers */
+canvas.addEventListener("mousemove", function (event) {
+    var pos = getPosT(event);
+    if (drawing) {
+        var pos = getPosT(event);
+        context.beginPath();
+        context.moveTo(oldPos.x, oldPos.y);
+        context.lineTo(pos.x, pos.y);
+        context.stroke();
+        context.closePath();
+        oldPos = pos;
+    }
+}, false);
+
+// tap start
+canvas.addEventListener("mousedown", function (event) {
+    drawing = true;
+    oldPos = getPosT(event);
+}, false);
+
+// tap fin
+canvas.addEventListener("mouseup", function () {
+    drawing = false;
+}, false);
+
