d9f4cd15722471ccb74ccc480e77845a5751399a
Koki Tanaka <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-07-19 11:15:18 +0900

    delete func

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* define valuable */
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext('2d');
-canvas.setAttribute("width", window.innerWidth );
+canvas.setAttribute("width", window.innerWidth);
 canvas.setAttribute("height", window.innerHeight);
 var drawing = false;
 var oldPos;
@@ -22,15 +22,15 @@ function scrollY() {
 }
 
 function getPosM(event) {
-    var mouseX = event.clientX ;
+    var mouseX = event.clientX;
     var mouseY = event.clientY;
     return { x: mouseX, y: mouseY };
 }
 
-function getPosT(eventname){
+function getPosT(eventname) {
     var touch = eventname.changedTouches[0];
-    var xpos = touch.pageX - canvas.offsetLeft-2;
-    var ypos = touch.pageY - canvas.offsetTop-2;
+    var xpos = touch.pageX - canvas.offsetLeft - 2;
+    var ypos = touch.pageY - canvas.offsetTop - 2;
     return { x: xpos, y: ypos };
 }
 
@@ -38,16 +38,15 @@ function getPosT(eventname){
 canvas.addEventListener("mousedown", function (event) {
     drawing = true;
     oldPos = getPosM(event);
-    alert(event.touches[0].touchType);
 
 }, false);
 canvas.addEventListener("touchstart", function (event) {
     oldPos = getPosT(event);
-    if(event.touches[0].touchType=="stylus"){
+    if (event.touches[0].touchType == "stylus") {
         drawing = true;
     }
-    else if(event.touches[0].touchType=="direct"){
-        drawing=false;
+    else if (event.touches[0].touchType == "direct") {
+        drawing = false;
     }
 
 }, false);
@@ -77,7 +76,7 @@ canvas.addEventListener("touchmove", function (event) {
         context.closePath();
         oldPos = pos;
     }
-},  {passive: false});
+}, { passive: false });
 
 // tap fin
 canvas.addEventListener("mouseup", function () {
@@ -89,20 +88,57 @@ canvas.addEventListener("touchend", function () {
 
 
 /* Pressurejs */
-Pressure.set( '#myCanvas', {
-    start: function(event){
-      },
-      end: function(){
-      },
-      startDeepPress: function(event){
-      },
-      endDeepPress: function(){
-      },
-      change: function(force, event){
-        context.lineWidth=3**(force);
-      },
-      unsupported: function(){
-      }
-    });
+Pressure.set('#myCanvas', {
+    start: function (event) {
+    },
+    end: function () {
+    },
+    startDeepPress: function (event) {
+    },
+    endDeepPress: function () {
+    },
+    change: function (force, event) {
+        context.lineWidth = 3 ** (force);
+    },
+    unsupported: function () {
+    }
+});
 
+//get finger swipe
+$(function () {
+    $('#main').on('touchstart', onTouchStart); //指が触れたか検知
+    $('#main').on('touchmove', onTouchMove); //指が動いたか検知
+    $('#main').on('touchend', onTouchEnd); //指が離れたか検知
+    var direction, position;
 
+    //スワイプ開始時の横方向の座標を格納
+    function onTouchStart(event) {
+        position = getPosition(event);
+        direction = ''; //一度リセットする
+    }
+
+    //スワイプの方向（left／right）を取得
+    function onTouchMove(event) {
+        if (event.touches[0].touchType == "direct") {
+        if (position - getPosition(event) > 70) { // 70px以上移動しなければスワイプと判断しない
+            direction = 'left'; //左と検知
+        } else if (position - getPosition(event) < -70) {  // 70px以上移動しなければスワイプと判断しない
+            direction = 'right'; //右と検知
+        }
+    }
+    }
+
+  function onTouchEnd(event) {
+    if (event.touches[0].touchType == "direct") {
+    if (direction == 'right'){
+     //delete
+     context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
+  }
+
+  //横方向の座標を取得
+  function getPosition(event) {
+    return event.originalEvent.touches[0].pageX;
+  }
+});
